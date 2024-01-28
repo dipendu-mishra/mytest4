5d43d176ffced0f8ba5979f9aa905b7189ce777f
dipendu-mishra <34467980+dipendu-mishra@users.noreply.github.com>
2024-01-28 23:10:34 +0530

    first commit

---
+++ src/main/java/MyPack1/MyCalc.java
@@ -0,0 +1,14 @@
+package MyPack1;
+
+public class MyCalc {
+
+	public int sum(int a , int b)
+	{
+		return a+b;
+	}
+	public static void main(String []args)
+	{
+		MyCalc myCalc=new MyCalc();
+		System.out.println(myCalc.sum(10, 20));
+	}
+}
